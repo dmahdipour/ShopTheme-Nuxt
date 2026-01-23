32af47a49337935286555af272c19a99da5aa1bd
dmahdipour <daruosh.mehdipour@gmail.com>
2026-01-23 14:06:30 +0330

    Feat: index page init

---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="page-wrapper">
+        <the-main-header></the-main-header>
+        <the-mobile-header></the-mobile-header>
+        <!-- start of page-content -->
+        <main class="page-content">
+            <div class="container">
+                <slot></slot>
+            </div>
+        </main>
+        <the-footer></the-footer>
+    </div>
+</template>
+
+<script setup lang="ts">
+    useHead({
+        link:[
+            {rel:"stylesheet", href:"/css/dependencies.css"},
+        ],
+        script:[
+            {src:"/js/dependencies/bootstrap-slider.min.js", body:true},
+            {src:"/js/dependencies/bootstrap.bundle.min.js", body:true},
+            {src:"/js/dependencies/fancybox.umd.js", body:true},
+            {src:"/js/dependencies/iziToast.min.js", body:true},
+            {src:"/js/dependencies/jquery-3.6.0.min.js", body:true},
+            {src:"/js/dependencies/jquery.countdown.min.js", body:true},
+            {src:"/js/dependencies/jquery.simple.timer.min.js", body:true},
+            {src:"/js/dependencies/nouislider.min.js", body:true},
+            {src:"/js/dependencies/remodal.min.js", body:true},
+            {src:"/js/dependencies/select2.min.js", body:true},
+            {src:"/js/dependencies/simplebar.min.js", body:true},
+            {src:"/js/dependencies/swiper-bundle.min.js", body:true},
+            {src:"/js/dependencies/wNumb.js", body:true},
+            {src:"/js/dependencies/zoomsl.min.js", body:true},
+            {src:"/js/theme.js", body:true},
+            {src:"/js/custom.js", body:true}
+        ]
+    })
+</script>
+
+<style>
+
+</style>
