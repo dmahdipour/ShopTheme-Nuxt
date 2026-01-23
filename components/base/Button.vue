5b103cb2c65fc074cef168435a4333a6fd057be5
dmahdipour <daruosh.mehdipour@gmail.com>
2026-01-23 20:25:33 +0330

    Feat: Btn Component

---
+++ components/base/Button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button :class="['btn', {'btn-sm': size=='sm'}, {'btn-lg': size=='lg'}, {'w-100': wFull==true}, outline ? `btn-outline-${color}` : `btn-${color}`, ]">
+        <slot></slot>
+        <slot name="icon"></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+    defineProps({
+        size:{
+            default: "normal",
+            type: String
+        },
+        wFull:{
+            default: false,
+            type: Boolean
+        },
+        outline:{
+            default: false,
+            type: Boolean
+        },
+        color:{
+            default: "primary",
+            type: String
+        }
+    })
+</script>
