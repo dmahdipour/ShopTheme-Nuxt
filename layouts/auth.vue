e1614c7fd76348728fb74ea53659485b3e5cbd37
dmahdipour <daruosh.mehdipour@gmail.com>
2026-02-10 20:44:17 +0330

    Fix: Authorization layout style

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,31 +1,14 @@
 <template>
     <div class="page-wrapper">
-        <header class="page-mini-header py-4">
-            <div class="d-flex justify-content-center">
-                <div class="logo-container">
-                    <nuxt-link to="/" class="logo">
-                        <img src="/images/logo-dark.svg" width="120" alt="logo">
-                    </nuxt-link>
-                </div>
-            </div>
-        </header>
         <main class="page-content page-auth">
             <slot></slot>
         </main>
         <footer class="page-mini-footer">
-            <div class="d-flex justify-content-center py-5">
-                <div class="logo-container">
-                    <nuxt-link to="/" class="logo">
-                        <img src="/images/logo-dark.svg" width="120" alt="logo">
-                    </nuxt-link>
-                </div>
-            </div>
             <div class="copy-right bg-light py-3">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-6 mb-md-0 mb-4">
-                            <div class="text-muted text-md-start text-center">کليه حقوق اين سايت متعلق به داریوش مهدی پور یقینی
-                                می‌باشد.
+                            <div class="text-muted text-md-start text-center">کليه حقوق اين سايت متعلق به داریوش مهدی پور یقینی می‌باشد.
                             </div>
                         </div>
                         <div class="col-md-6">
